1746e39407832b1699f60ac385c5f9298e789e23
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-22 12:24:16 -0300

    finish about.js

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## TODO:
 - ~~Finish `menu.js`~~
-- Finish `about.js`
+- ~~Finish `about.js`~~
 - Host on gh-pages [Guide here](https://gist.github.com/cobyism/4730490)
--- src/about.js
+++ src/about.js
@@ -1,4 +1,6 @@
 import appendElement from "./appendElement";
 
 export default function renderAbout(contentDiv) {
+  const aboutText = appendElement(contentDiv, "h3", ["about-text"])
+  aboutText.textContent = "Made by Manuel Garcia for The Odin Project."
 }
