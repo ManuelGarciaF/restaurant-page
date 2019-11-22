f63eabdb84979fb8524eae505e39adff953455bf
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-22 12:12:51 -0300

    make selected class move to selected tab

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import renderRestaurant from "./restaurant";
 import renderMenu from "./menu";
 import renderAbout from "./about";
 
+// https://plainjs.com/javascript/attributes/adding-removing-and-testing-for-classes-9/
+function addClass(el, className) {
+  if (el.classList) el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += " " + className;
+}
+
+function removeClass(el, className) {
+  if (el.classList) el.classList.remove(className);
+  else
+    el.className = el.className.replace(
+      new RegExp("\\b" + className + "\\b", "g"),
+      ""
+    );
+}
+
 function renderTab(tabId) {
   const contentDiv = document.getElementById("content");
   contentDiv.innerHTML = "";
@@ -23,6 +38,13 @@ const tabButtons = document.querySelectorAll(".tab");
 tabButtons.forEach(elem => {
   elem.addEventListener("click", event => {
     renderTab(Number(event.target.getAttribute("data-index")));
+
+    // Add `selected` class to clicked on tab and remove it from all others.
+    const tabButtons = event.target.parentElement.children;
+    for (let i = 0; i < tabButtons.length; i++) {
+      removeClass(tabButtons[i], "selected");
+    }
+    addClass(event.target, "selected");
   });
 });
 
